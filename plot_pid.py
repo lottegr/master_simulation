9fc037045b85c16b9d4d691066d2da2c61a5d4ed
Lotte <lottegr@gmail.com>
2022-06-01 13:58:13 +0200

    010622

--- plot_pid.py
+++ plot_pid.py
@@ -9,10 +9,10 @@ import numpy as np
 lab = ["P =1", "P = 0.5", "P = 0.25"]
 j = -1
 # for i in range(5,10):
-for i in [18,20]:
+for i in [4,9]:
     j += 1
     plot_nr = str(i)
-    ref = 1
+    ref = 0
 
     xlab = 'Position [m]'
     ulab = 'Angular velocity [rad/s]'
@@ -23,35 +23,35 @@ for i in [18,20]:
     # ------------
 
     x = []; u = []; p = []
-    # x_file = "sensor" + plot_nr + ".txt"
-    # u_file = "input" + plot_nr + ".txt"
-
-    x_file = "pose" + plot_nr + ".txt"
+    x_file = "sensor" + plot_nr + ".txt"
     u_file = "input" + plot_nr + ".txt"
-    p_file = "sensor" + plot_nr + ".txt"
+
+    # x_file = "pose" + plot_nr + ".txt"
+    # u_file = "input" + plot_nr + ".txt"
+    # p_file = "sensor" + plot_nr + ".txt"
 
     x_ = open(x_file,'r')
     u_ = open(u_file,'r')
-    p_ = open(p_file,'r')
+    # p_ = open(p_file,'r')
     lines_x = x_.readlines()
     lines_u = u_.readlines()
-    lines_p = p_.readlines()
+    # lines_p = p_.readlines()
     for l in lines_x:
         if abs(float(l)) > 1e-100:
-            x.append(float(l))
+            x.append(float(l)-1)
     for l in lines_u:
         if abs(float(l)) > 1e-100:
             u.append(float(l))
-    for l in lines_p:
-        if abs(float(l)) > 1e-100:
-            p.append(float(l))
+    # for l in lines_p:
+    #     if abs(float(l)) > 1e-100:
+    #         p.append(float(l)-1)
 
 
     # plt.figure(i)
     plt.subplot(1,2,1)
     # plt.plot(range(len(x)),x, label=lab[j])
     plt.plot(range(len(x)),x, label="Pose of base")
-    plt.plot(range(len(p)),p, label="Pose of p")
+    # plt.plot(range(len(p)),p, label="Pose of p")
     plt.ylabel(xlab)
     plt.axhline(ref,color='grey')
     plt.title('Measurements')
